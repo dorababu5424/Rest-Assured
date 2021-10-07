910de2df945c604dbd49e253d24821a569c516e4
Dorababu <dorababu5424@gmail.com>
2021-10-07 14:14:05 +0530

    Rest assured

---
+++ src/test/java/RestPackage/Base.java
@@ -0,0 +1,61 @@
+package RestPackage;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.Assert;
+import static io.restassured.RestAssured.*;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.ValidatableResponse;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.apache.http.entity.mime.content.StringBody;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.*;
+public class Base {
+
+	@Test
+	private void Tc1() {
+		// TODO Auto-generated method stub
+
+	
+		//given= all input details
+		//when ==submiting apis
+		//then validating
+		RestAssured.baseURI= "https://rahulshettyacademy.com";
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body("{\r\n"
+				+ "  \"location\": {\r\n"
+				+ "    \"lat\": -38.383494,\r\n"
+				+ "    \"lng\": 33.427362\r\n"
+				+ "  },\r\n"
+				+ "  \"accuracy\": 50,\r\n"
+				+ "  \"name\": \"Frontline house\",\r\n"
+				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
+				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
+				+ "  \"types\": [\r\n"
+				+ "    \"shoe park\",\r\n"
+				+ "    \"shop\"\r\n"
+				+ "  ],\r\n"
+				+ "  \"website\": \"http://google.com\",\r\n"
+				+ "  \"language\": \"French-IN\"\r\n"
+				+ "}\r\n"
+				+ "").when().post("/maps/api/place/add/json").then().log().all().assertThat().statusCode(200));
+				
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
